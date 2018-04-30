26382139974429dcf98dc0934c9c31871558e350
Cody Sehl <cody.sehl@gmail.com>
2018-04-29 19:04:47 -0600

    more diagnosing

--- travis/build.sh
+++ travis/build.sh
@@ -3,8 +3,4 @@
 # Builds junk
 
 echo '### Building hexo site ###'
-echo "#### node_modules/hexo/bin "
-ls node_modules/hexo/bin
-echo "#### node_modules/.bin"
-ls node_modules/.bin
-node_modules/hexo/bin/hexo generate --config _config-prod.yml --debug
+node_modules/.bin/hexo generate --config _config-prod.yml --debug
