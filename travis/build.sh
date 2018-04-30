05fbfb9a0c73259efc03c76284b707be72813ced
Cody Sehl <cody.sehl@gmail.com>
2018-04-29 18:08:01 -0600

    diagnose build errors

--- travis/build.sh
+++ travis/build.sh
@@ -3,4 +3,8 @@
 # Builds junk
 
 echo '### Building hexo site ###'
-./node_modules/hexo/bin/hexo generate --config _config-prod.yml --debug
+echo "#### node_modules/hexo/bin"
+ls node_modules/hexo/bin
+echo "#### node_modules/.bin"
+ls node_modules/.bin
+node_modules/hexo/bin/hexo generate --config _config-prod.yml --debug
